cb31a67f71d33a02b2a6bd0dcbaaba98de7165f0
Gift Jacksun <codewithjacksun@gmail.com>
2026-05-11 23:07:10 +0100

    Remove App.css file and update AuthLayout component for improved structure; add custom scrollbar styles in index.css.

--- src/layout/auth-layout.tsx
+++ src/layout/auth-layout.tsx
@@ -12,24 +12,22 @@ export default function AuthLayout({
   description,
 }: AuthLayoutProps) {
   return (
-    
-      <section className="relative">
-        <div className="absolute top-4 right-4">
-          <ModeToggle />
+    <section>
+      <div className="fixed top-4 right-4 z-10">
+        <ModeToggle />
+      </div>
+      <div className="max-w-[400px] w-[90%] mx-auto py-20 space-y-6">
+        <div className="center">
+          <Link to="/">
+            <img src="/logo.svg" alt="logo" className="w-10 h-10" />
+          </Link>
         </div>
-        <div className="max-w-[400px] w-[90%] mx-auto py-20 space-y-6">
-          <div className="center">
-            <Link to="/">
-              <img src="/logo.svg" alt="logo" className="w-10 h-10" />
-            </Link>
-          </div>
-          <div className="text-center">
-            <h1 className="text-2xl font-bold font-sans">{title}</h1>
-            <p className="text-sm text-muted">{description}</p>
-          </div>
-          <div className="space-y-4">{children}</div>
+        <div className="text-center">
+          <h1 className="text-2xl font-bold font-sans">{title}</h1>
+          <p className="text-sm text-muted">{description}</p>
         </div>
-      </section>
-   
+        <div className="space-y-4">{children}</div>
+      </div>
+    </section>
   );
 }
